bd96c49dc7eed61844e9194c010fb4ba349fdb2c
Satvik Sharma <87787530+sharmasatvik989@users.noreply.github.com>
2025-01-19 17:44:19 -0600

    Toggle dark-mode switch

---
+++ darkmode.js
@@ -0,0 +1,19 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeswitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode','active')
+}
+
+const disableDarKmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode','null')
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeswitch.addEventListener("click",() => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarKmode()
+})
